36bd3edb565cd776926c60a54ed633c62b815883
Denise Noemi Rea Diaz <105187806+DeniseRea@users.noreply.github.com>
2025-11-03 22:57:26 -0500

    Estructura atomic design

---
+++ atomic_design/lib/design/atoms/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF22577A);
+  static const Color secondary = Color(0xFF38A3A5);
+  static const Color accent = Color(0xFF57CC99);
+  static const Color lightGreen = Color(0xFF80ED99);
+  static const Color background = Color(0xFFC7F9CC);
+}
